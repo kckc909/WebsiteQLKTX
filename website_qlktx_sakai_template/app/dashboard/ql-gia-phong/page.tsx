6842d465d93463e2eeb213c1994e5d341f70faff
Minh <kckc253261@gmail.com>
2025-05-07 17:11:00 +0700

    chore : Chấm trước công việc

---
+++ website_qlktx_sakai_template/app/dashboard/ql-gia-phong/page.tsx
@@ -0,0 +1,7 @@
+// hiển thị giá phòng đang áp dụng hiện tại -> các loại phòng đặc biệt
+// Thêm sửa xóa giá phòng
+// hiển thị danh sách giá phòng -> chọn cả loại phòng
+
+const QlGiaPhongPage = () => {
+}
+export default QlGiaPhongPage;
